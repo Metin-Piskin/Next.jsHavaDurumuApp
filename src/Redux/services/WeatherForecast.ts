dc0ea447d81fbe93a617b7139b3010ce4502187c
Metin Pişkin <85956297+Metin-Piskin@users.noreply.github.com>
2024-08-05 16:34:12 +0300

    Update WeatherForecast.ts

--- src/Redux/services/WeatherForecast.ts
+++ src/Redux/services/WeatherForecast.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const dayWeatherForecastApi = createApi({
   reducerPath: "dayWeatherForecastApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://api.openweathermap.org/data/2.5/",
+    baseUrl: "https://api.openweathermap.org/data/2.5/",
   }),
   endpoints: (builder) => ({
     getWeatherForecast: builder.query({
